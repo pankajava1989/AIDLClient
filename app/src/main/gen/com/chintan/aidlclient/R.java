1187b2b59394e8a38a6ef35c168706b159f92805
pankajava1989 <pankajkataria786@gmail.com>
2019-07-12 14:09:07 +0530

    Initial commit

---
+++ app/src/main/gen/com/chintan/aidlclient/R.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package com.chintan.aidlclient;
+
+/* This stub is only used by the IDE. It is NOT the R class actually packed into the APK */
+public final class R {
+}
